4352b70796eb13bd305a364628fc650f5a02752b
floroz <tortora87@gmail.com>
2019-05-16 13:23:19 +0100

    Product component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
 	render() {
 		return (
-			<div>
-				<Product />
-			</div>
+			<>
+				<div className='py-5'>
+					<div className='container'>
+						<Title name='Our' title='Products' />
+
+						<div className='row'>
+							<ProductConsumer>
+								{value => {
+									return value.products.map(prod => {
+										return <Product product={prod} key={prod.id} />;
+									});
+								}}
+							</ProductConsumer>
+						</div>
+					</div>
+				</div>
+			</>
 		);
 	}
 }
