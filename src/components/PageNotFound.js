f9a60cb4bd2958aa41cb662ecba2809e5ff51119
floroz <tortora87@gmail.com>
2019-05-16 19:33:02 +0100

    page not found create layout

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 export default class PageNotFound extends Component {
 	render() {
+		const { location } = this.props;
 		return (
-			<div>
-				<h3>Page Not Found</h3>
+			<div className="container">
+				<div className="row">
+					<div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+						<h1 className="display-3">404</h1>
+						<h1>Error</h1>
+						<h2>page not found</h2>
+						<h3>
+							the requested url{' '}
+							<span className="text-danger">{location.pathname}</span> is not
+							found
+						</h3>
+					</div>
+				</div>
 			</div>
 		);
 	}
